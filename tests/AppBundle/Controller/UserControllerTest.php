e3fcefaef055d8a596e62e2801dcf63024fea6e2
Lucio Stoco <luciostocosites@gmail.com>
2017-07-28 18:33:27 +0100

    teste done

--- tests/AppBundle/Controller/UserControllerTest.php
+++ tests/AppBundle/Controller/UserControllerTest.php
@@ -12,13 +12,34 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class UserControllerTest extends WebTestCase
 {
-    public function testIndex()
+    public function testNewUserAction()
     {
+
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/user/index');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $crawler = $client->request(
+            'GET',
+            '/user',
+            array(),
+            array(),
+            array('CONTENT_TYPE' => 'application/json'),
+            ''
+        );
+
+        $response = $client->getResponse();
+
+        // Test if response is OK
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        // Test if Content-Type is valid application/json
+        $this->assertSame('application/json', $response->headers->get('Content-Type'));
+        // Test if company was inserted
+        //$this->assertEquals('{"success":"true"}', $response->getContent());
+        // Test that response is not empty
+        $this->assertNotEmpty($client->getResponse()->getContent());
+
+        //$this->assertEquals(200, $client->getResponse()->getStatusCode());
         //$this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
+
     }
 }
